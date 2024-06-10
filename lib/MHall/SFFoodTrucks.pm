c6421efa29e926918c52ebe2c67779847deb85b8
Miller Hall <miller.hall@gmail.com>
2024-06-09 17:35:25 -0700

    lib/MHall/SFFoodTrucks.pm - Package stub

---
+++ lib/MHall/SFFoodTrucks.pm
@@ -0,0 +1,16 @@
+package MHall::SFFoodTrucks;
+
+use strict;
+use warnings;
+use v5.16;
+
+BEGIN {
+    our @EXPORT    = qw();
+    our @EXPORT_OK = qw();
+}
+
+use Exporter q(import);
+
+1;
+
+__END__
